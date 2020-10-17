e59dc8568cb67ff13dec280307b31b8bec88ea21
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2020-10-17 18:05:52 +0100

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,50 @@
-"use strict";
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+'use strict';
+
+const path = require('path');
 
 module.exports = {
-    // generate source map
-    devtool: 'source-map',
+  // generate source map
+  devtool: 'source-map',
 
-    // bundling mode
-    mode: 'production',
+  // bundling mode
+  mode: 'production',
 
-    // entry files
-    entry: './src/index.ts',
+  // entry files
+  entry: path.resolve(__dirname, './src/index.ts'),
 
-    // output bundles (location)
-    output: {
-        path: `./dist`,
-        filename: 'bundle.js',
-    },
+  // output bundles (location)
+  output: {
+    path: path.resolve(__dirname, './dist/bundle'),
+    filename: 'bundle.js',
+  },
 
-    // file resolutions
-    resolve: {
-        extensions: [
-          '.js',
-          '.ts',
-          '.tsx'
-        ],
-    },
+  // file resolutions
+  resolve: {
+    extensions: [
+      '.js',
+      '.ts',
+      '.tsx'
+    ],
+  },
 
-    // loaders
-    module: {
-        rules: [
-            {
-                test: /\.tsx?/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
-        ]
-    }
+  // loaders
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              '@babel/preset-env',
+              "@babel/preset-typescript"
+            ]
+          }
+        }
+      }
+    ],
+  }
 };
