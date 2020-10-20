5a2dc33899c8af4d8a4226f4a822f32da38ea367
Peter Hughes <github@phugh.es>
2020-10-20 16:54:08 +0100

    amend index.ts now --isolatedModules = true

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-// empty file to avoid "No inputs were found in config file." Typescript error
+// placeholder file to avoid "No inputs were found in config file." Typescript error
+export const test = Promise.allSettled([console.log(1)]);
