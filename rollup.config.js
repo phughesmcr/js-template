18bf1a8bd9f162d3a91fedea9d841da83d8b5248
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2021-06-30 19:31:23 +0100

    Update

--- rollup.config.js
+++ rollup.config.js
@@ -5,14 +5,14 @@ import commonjs from '@rollup/plugin-commonjs';
 import dts from "rollup-plugin-dts";
 import fs from 'fs';
 import * as pkg from './package.json';
-import {nodeResolve} from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-import { DEFAULT_EXTENSIONS } from '@babel/core';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from "rollup-plugin-terser";
+import { DEFAULT_EXTENSIONS } from '@babel/core';
 
 const license = fs.readFileSync('./LICENSE', 'utf-8').split(/\r?\n/g).reduce((str, line) => str += ` * ${line}\n`, '');
 
-const pkgName = pkg.name.split('/').pop().replace(/-/g, '');
+const pkgName = pkg.name;
 const pkgVersion = pkg.version;
 const extensions = [...DEFAULT_EXTENSIONS, '.ts', '.tsx'];
 
@@ -41,33 +41,39 @@ export default [
     external,
 
     plugins: [
-      // Allows node_modules resolution
       nodeResolve({
         extensions,
-        mainFields: ['jsnext:main'],
+        mainFields: ['module', 'main'],
       }),
 
-      // Allow bundling cjs modules. Rollup doesn't understand cjs
       commonjs({
         include: 'node_modules/**',
         transformMixedEsModules: true,
        }),
 
-      // Compile TypeScript/JavaScript files
       typescript({
-        clean: true,
         exclude: [ "node_modules", "*.d.ts", "**/*.d.ts" ],
         include: [ "*.ts+(|x)", "**/*.ts+(|x)", "*.m?js+(|x)", "**/*.m?js+(|x)" ],
+        module: "ES2020",
         tsconfig: "tsconfig.json",
         tslib: require('tslib'),
         typescript: require("typescript"),
       }),
 
+      babel({
+        extensions,
+        babelHelpers: 'bundled',
+        include: ["src/**/*"],
+        exclude: ["node_modules/**/*"],
+      }),
+
       terser({
-        ecma: 2021,
+        ecma: 2020,
         module: true,
         keep_classnames: true,
         keep_fnames: true,
+        compress: true,
+        mangle: true,
       }),
     ],
 
@@ -92,7 +98,7 @@ export default [
       // Allows node_modules resolution
       nodeResolve({
         extensions,
-        mainFields: ['main'],
+        mainFields: ['main', 'node'],
       }),
 
       // Allow bundling cjs modules. Rollup doesn't understand cjs
@@ -103,18 +109,24 @@ export default [
 
       // Compile TypeScript/JavaScript files
       typescript({
-        clean: true,
         exclude: [ "node_modules", "*.d.ts", "**/*.d.ts" ],
         include: [ "*.ts+(|x)", "**/*.ts+(|x)", "*.m?js+(|x)", "**/*.m?js+(|x)" ],
         tsconfig: "tsconfig.json",
-        module: "cjs",
         tslib: require('tslib'),
         typescript: require("typescript"),
       }),
 
+      babel({
+        extensions,
+        babelHelpers: 'bundled',
+        include: ["src/**/*"],
+        exclude: ["node_modules/**/*"],
+      }),
+
       terser({
-        keep_classnames: true,
-        keep_fnames: true,
+        ecma: 2020,
+        compress: true,
+        mangle: true,
       }),
     ],
 
@@ -124,6 +136,7 @@ export default [
       exports: 'named',
       file: pkg.main,
       format: 'cjs',
+      name: pkgName,
       sourcemap: true,
       globals,
     },
@@ -136,9 +149,24 @@ export default [
     external,
 
     plugins: [
-      nodeResolve({ extensions }),
+      nodeResolve({
+        extensions,
+        mainFields: ['browser', 'main'],
+        browser: true,
+      }),
 
-      commonjs({ include: 'node_modules/**' }),
+      commonjs({
+        include: 'node_modules/**',
+        transformMixedEsModules: true,
+      }),
+
+      typescript({
+        exclude: [ "node_modules", "*.d.ts", "**/*.d.ts" ],
+        include: [ "*.ts+(|x)", "**/*.ts+(|x)", "*.m?js+(|x)", "**/*.m?js+(|x)" ],
+        tsconfig: "tsconfig.json",
+        tslib: require('tslib'),
+        typescript: require("typescript"),
+      }),
 
       babel({
         extensions,
@@ -147,19 +175,24 @@ export default [
         exclude: ["node_modules/**/*"],
       }),
 
-      terser(),
+      terser({
+        ecma: 2020,
+        compress: true,
+        mangle: true,
+      }),
     ],
 
-    output: [{
+    output: {
       banner: bannerText,
       esModule: false,
       exports: 'named',
       file: "./dist/umd/index.min.js",
       format: 'umd',
       name: pkgName,
+      noConflict: true,
       sourcemap: true,
       globals,
-    }]
+    }
   },
 
   // BROWSER IIFE
@@ -171,12 +204,21 @@ export default [
     plugins: [
       nodeResolve({
         extensions,
+        mainFields: ['browser', 'main'],
         browser: true,
       }),
 
       commonjs({
         include: 'node_modules/**',
         transformMixedEsModules: true,
+      }),
+
+      typescript({
+        exclude: [ "node_modules", "*.d.ts", "**/*.d.ts" ],
+        include: [ "*.ts+(|x)", "**/*.ts+(|x)", "*.m?js+(|x)", "**/*.m?js+(|x)" ],
+        tsconfig: "tsconfig.json",
+        tslib: require('tslib'),
+        typescript: require("typescript"),
       }),
 
       babel({
@@ -187,13 +229,13 @@ export default [
       }),
 
       terser({
-        module: false,
-        keep_classnames: true,
-        keep_fnames: true,
+        ecma: 2020,
+        compress: true,
+        mangle: true,
       }),
     ],
 
-    output: [{
+    output: {
       banner: bannerText,
       esModule: false,
       exports: 'named',
@@ -202,7 +244,7 @@ export default [
       name: pkgName,
       sourcemap: true,
       globals,
-    }],
+    },
   },
 
   // TYPESCRIPT DECLARATIONS
@@ -211,25 +253,21 @@ export default [
     output: [
       // CJS
       {
-        banner: bannerText,
         file: pkg.types,
         format: "es"
       },
       // UMD
       {
-        banner: bannerText,
         file: "./dist/umd/index.min.d.ts",
         format: "es"
       },
       // Browser
       {
-        banner: bannerText,
         file: "./dist/iife/index.min.d.ts",
         format: "es"
       },
       // Module
       {
-        banner: bannerText,
         file: "./dist/esm/index.min.d.ts",
         format: "es"
       },
